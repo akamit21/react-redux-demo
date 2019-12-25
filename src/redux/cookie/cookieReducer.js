784ef97eea2dab528a64d517e271efc9e52ae12f
Amit Kumar <akamit21@outlook.com>
2019-12-26 01:14:28 +0530

    paylaod added

--- src/redux/cookie/cookieReducer.js
+++ src/redux/cookie/cookieReducer.js
@@ -9,7 +9,7 @@ const cookieReducer = (state = initialState, action) => {
     case BUY_COOKIE:
       return {
         ...state,
-        numOfCookies: state.numOfCookies - 1
+        numOfCookies: state.numOfCookies - action.payload
       };
     default:
       return state;
