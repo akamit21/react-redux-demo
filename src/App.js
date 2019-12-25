5573720fb7a3fe1fe9d740b56be855bbb480ab5b
Amit Kumar <akamit21@outlook.com>
2019-12-26 01:13:28 +0530

    new component added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import CakeContainer from "./component/CakeContainer";
 import HooksCakeContainer from "./component/HooksCakeContainer";
-
+import BuyCookieContainer from "./component/BuyCookieContainer";
 import CookieContainer from "./component/CookieContainer";
 import "./App.css";
 
@@ -14,6 +14,7 @@ function App() {
         <CakeContainer />
         <HooksCakeContainer />
         <CookieContainer />
+        <BuyCookieContainer />
       </div>
     </Provider>
   );
